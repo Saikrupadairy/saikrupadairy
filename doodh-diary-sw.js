4c47b0d795d64d90abb46d8a4e56348cd09cadbd
Saikrupadairy <yashkolhe06@gmail.com>
2026-04-03 20:44:20 +0530

    Add files via upload

---
+++ doodh-diary-sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'doodh-diary-v1';
+const CDN = [
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => {
+      c.addAll(['./','./index.html']).catch(()=>{});
+      CDN.forEach(url => fetch(url,{mode:'no-cors'}).then(r=>c.put(url,r)).catch(()=>{}));
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if(res && res.status === 200) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
